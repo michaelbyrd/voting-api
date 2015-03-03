0b65d2edd3882a2f4828391938418e2ef9bd05cb
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-03 17:25:31 -0500

    add open? to json and add remaming time to json(displays negtive number if poll is closed)

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,7 +3,27 @@ class Race < ActiveRecord::Base
   has_many :votes
 
   def as_json(options = {})
-    { title: title, vote_count: votes.count, candidates: candidates }
+    { title: title, vote_count: votes.count, open: open?,
+      time_remaining: display_remaining_time,
+      candidates: candidates }
   end
 
+  def open?
+    DateTime.now < self.end_at
+  end
+
+  private def display_remaining_time
+    time_passed
+  end
+
+  def time_passed
+    difference = self.end_at - DateTime.now
+    [:day, :hour, :minute].each do |unit|
+      if difference >= 1.send(unit)
+        time = (difference / 1.send(unit)).round
+        return "#{time} #{unit}#{time==1 ? '' : 's'} remaining"
+      end
+    end
+    return "#{difference} seconds remaining"
+  end
 end
--- test/models/race_test.rb
+++ test/models/race_test.rb
@@ -4,6 +4,7 @@ class RaceTest < ActiveSupport::TestCase
   test "voters can only vote once per race" do
     race = races(:one)
     voter = Voter.create(name: "Bozo", party: "TheClown")
+    
     v1 = Vote.create(race: race, voter: voter, candidate: race.candidates.first)
     v2 = Vote.create(race: race, voter: voter, candidate: race.candidates.first)
 
