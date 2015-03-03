ab1291d95aac80dfd04513c5bdfe2db74216f5bc
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-03 17:13:31 -0500

    add race test voters can only vote once per race

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,12 +3,17 @@ class Vote < ActiveRecord::Base
   belongs_to :candidate
   belongs_to :race
 
-  validates :voter_id, presence: true
+  validates :voter_id, uniqueness: true
   validates :candidate_id, presence: true
   validates :race_id, presence: true
-
 
   def as_json(options = {})
     { voter: voter.name, candidate: candidate.name }
   end
+
+  # private def voters_can_vote_once_per_race
+  #   if self.race.votes.find_by(voter_id: )
+  #    errors.add(:vote, "can't vote twice in the same race")
+  #  end
+  # end
 end
--- test/models/race_test.rb
+++ test/models/race_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class RaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "voters can only vote once per race" do
+    race = races(:one)
+    voter = Voter.create(name: "Bozo", party: "TheClown")
+    v1 = Vote.create(race: race, voter: voter, candidate: race.candidates.first)
+    v2 = Vote.create(race: race, voter: voter, candidate: race.candidates.first)
+
+    assert v1.valid?
+    refute v2.valid?
+  end
 end
