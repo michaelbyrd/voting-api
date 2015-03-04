08f54405982db9926bb3383d412a34a9d1e3bc79
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-03 20:23:34 -0500

    Api now needs header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
 
+  protect_from_forgery with: :null_session
   before_action :restrict_access
 
-  private def restrict_access
-     api_key = ApiKey.find_by_access_token(params[:access_token])
-     head :unauthorized unless api_key
-   end
+  # private def restrict_access
+  #    api_key = ApiKey.find_by_access_token(params[:access_token])
+  #    head :unauthorized unless api_key
+  #  end
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
+  end
 end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  protect_from_forgery with: :exception, only: :index
+  skip_before_action :restrict_access, only: :index
+
+  def index
+    # puts ENV['VOTING_API_TOKEN']
+    # @races = RaceRequest.new.races
+    # @races = []
+    @races = HTTParty.get("http://localhost:3000/api/v1/votes/",
+                            :headers => {
+                            "access_token" => ENV['VOTING_API_TOKEN']})
+  end
+end
